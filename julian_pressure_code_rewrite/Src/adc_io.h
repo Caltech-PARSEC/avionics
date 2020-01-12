b4c84fe2ae040470de355ce0fbc9820bd5eee878
Julian Sanders <jmsander@caltech.edu>
2020-01-11 19:35:21 -0800

    Started writing a DMA version of pressure code
    Mostly only generated code. Also defined some IO constants

---
+++ julian_pressure_code_rewrite/Src/adc_io.h
@@ -0,0 +1,18 @@
+/*
+This file contains constants mapping the pressure transducer numbers (labelled
+on PCB silkscreen) to ADC channels.
+
+Revision History:
+ 11 Jan 2020	Julian Sanders	Initial Revision
+
+*/
+
+#define PT1_ADC_CHAN ADC_CHANNEL_0
+#define PT2_ADC_CHAN ADC_CHANNEL_1
+#define PT3_ADC_CHAN ADC_CHANNEL_2
+#define PT4_ADC_CHAN ADC_CHANNEL_6
+#define PT5_ADC_CHAN ADC_CHANNEL_7
+#define PT6_ADC_CHAN ADC_CHANNEL_10
+#define PT7_ADC_CHAN ADC_CHANNEL_11
+#define PT8_ADC_CHAN ADC_CHANNEL_12
+#define PT9_ADC_CHAN ADC_CHANNEL_14
