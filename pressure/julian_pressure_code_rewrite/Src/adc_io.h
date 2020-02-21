2d95ec5993af72c03e31c66b3d8fc8d846a5a7bf
Julian Sanders <jmsander@caltech.edu>
2020-02-20 18:54:24 -0800

    Reorganized a bunch of folders

--- pressure/julian_pressure_code_rewrite/Src/adc_io.h
+++ pressure/julian_pressure_code_rewrite/Src/adc_io.h
@@ -4,15 +4,17 @@ on PCB silkscreen) to ADC channels.
 
 Revision History:
  11 Jan 2020	Julian Sanders	Initial Revision
+ 20 Feb 2020	Julian Sanders	Added port designator in comments
 
 */
 
-#define PT1_ADC_CHAN ADC_CHANNEL_0
-#define PT2_ADC_CHAN ADC_CHANNEL_1
-#define PT3_ADC_CHAN ADC_CHANNEL_2
-#define PT4_ADC_CHAN ADC_CHANNEL_6
-#define PT5_ADC_CHAN ADC_CHANNEL_7
-#define PT6_ADC_CHAN ADC_CHANNEL_10
-#define PT7_ADC_CHAN ADC_CHANNEL_11
-#define PT8_ADC_CHAN ADC_CHANNEL_12
-#define PT9_ADC_CHAN ADC_CHANNEL_14
+//										   Port Designator
+#define PT1_ADC_CHAN ADC_CHANNEL_0		// PA0
+#define PT2_ADC_CHAN ADC_CHANNEL_1		// PA1
+#define PT3_ADC_CHAN ADC_CHANNEL_2		// PA2
+#define PT4_ADC_CHAN ADC_CHANNEL_6		// PA6
+#define PT5_ADC_CHAN ADC_CHANNEL_7		// PA7
+#define PT6_ADC_CHAN ADC_CHANNEL_10		// PC0
+#define PT7_ADC_CHAN ADC_CHANNEL_11		// PC1
+#define PT8_ADC_CHAN ADC_CHANNEL_12		// PC2
+#define PT9_ADC_CHAN ADC_CHANNEL_14		// PC4
